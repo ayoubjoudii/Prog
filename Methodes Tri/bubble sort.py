bac50859bfb647e40d6c79831c8a6ea2873cf261
ayoub <ayoub.joudi556@gmail.com>
2024-06-01 13:35:55 +0100

    modified:   Methodes Tri/bubble sort.py

--- Methodes Tri/bubble sort.py
+++ Methodes Tri/bubble sort.py
@@ -3,15 +3,14 @@ from tableau import *
 t = exemp()
 print(t)
 def bulle(t,n):
-    for i in range(n):
-        print(i)
+    for i in range(n): 
+        print("\n",i,"\n")
         for j in range(n-1):
-            print("old",t[j],t[j+1])
+            print(t[j],end=" ")
             if t[j] > t[j+1]:
                 temp = t[j]
                 t[j] = t[j+1]
                 t[j+1] = temp
-                print("new",t[j],t[j+1])
-    # print(t)
+        print("\n",t)
 
 bulle(t,20)
