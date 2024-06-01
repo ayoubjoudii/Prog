9d6440291f949ce70a11ffde4166cfb427e3c3f2
ayoub <ayoub.joudi556@gmail.com>
2024-06-01 16:15:46 +0100

    modified:   Matrices/diagonal left.py

--- Matrices/diagonal left.py
+++ Matrices/diagonal left.py
@@ -5,23 +5,34 @@ for i in range(n):
     print("\t")
     for j in range(n//2):
         print(m[i,j],end=" ")
-print("\n","half top")
+print("\n\n","half top")
 for i in range(n//2):
     print("\t")
     for j in range(n):
         print(m[i,j],end=" ")
-print("\n","half bottom")
+print("\n\n","half bottom")
 for i in range(n//2,n):
     print("\t")
     for j in range(n):
         print(m[i,j],end=" ")
-print("\n","half top left")
+print("\n\n","half top left")
 for i in range(n//2):
     print("\t")
     for j in range(n//2):
         print(m[i,j],end=" ")
-print("\n","half bottom left")
+print("\n\n","half bottom left")
 for i in range(n//2,n):
     print("\t")
     for j in range(n//2):
         print(m[i,j],end=" ")
+print("\n\n","bottom left diagonal")
+for i in range(n):
+    print("\t")
+    for j in range(i+1):
+        print(m[i,j],end=" ")
+print("\n\n","top left diagonal")
+for i in range(n):
+    print("\t")
+    for j in range(n-i):
+        print(m[i,j],end=" ")
+
